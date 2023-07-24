5341e6e981139c25e3cd9c31da1f86a250143ded
김민서 <blackcode-@naver.com>
2023-07-24 23:47:48 +0900

    Start SpringBoot Project

---
+++ BLACKCODE/src/main/java/com/blackcode/www/BlackcodeApplication.java
@@ -0,0 +1,13 @@
+package com.blackcode.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackcodeApplication.class, args);
+	}
+
+}
